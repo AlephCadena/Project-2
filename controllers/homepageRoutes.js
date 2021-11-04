6ef38607236636c700eb4a25e324eb517995b33a
Aleph Cadena <alephcadena@gmail.com>
2021-11-04 10:27:29 -0500

    uodated user login

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 
 
 router.get('/', async (req, res) => {
- res.render('homepage');
+    res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
   });
   
   module.exports = router;
